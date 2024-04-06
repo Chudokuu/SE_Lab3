76371d714ff16cc57205c950c3ac00992817f9f6
chudokuu <katshutyan@mail.ru>
2024-04-06 16:23:15 +0300

    Привет

---
+++ SE_Lab3_1/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class Server
+{
+    static async Task Main(string[] args)
+    {
+        IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, 8888);
+        using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        socket.Bind(ipPoint);
+        socket.Listen();
+        Console.WriteLine("Server is running\n");
+
+        while (true)
+        {
+            await WaitForConnection(socket);
+        }
+    }
+
+    static async Task WaitForConnection(Socket socket)
+    {
+        Console.WriteLine("Waiting for connect...");
+        using Socket client = await socket.AcceptAsync();
+        Console.WriteLine($"Client connected. Client address: {client.RemoteEndPoint}");
+
+        byte[] buffer = new byte[512];
+        int bytesReceived = await client.ReceiveAsync(buffer, SocketFlags.None);
+        string data = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+        Console.WriteLine($"Data received from client: {data}");
+
+        string response = "";
+
+        if (data == "exit")
+        {
+            Console.WriteLine("Exit command received. Server is shutting down.");
+            Environment.Exit(0);
+        }
+
+        string[] request = data.Split(' ');
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+        if (request[0] == "PUT"  || request[0] == "GET" ||  request[0] == "DELETE")
+        {
+            string filepath = Path.Combine(desktopPath, "data", request[1]);
+
+            if (request[0] == "PUT")
+            {
+                DirectoryInfo directory = Directory.CreateDirectory(Path.Combine(desktopPath, "data"));
+                FileInfo file = new FileInfo(filepath);
+                if (file.Exists)
+                {
+                    response += "403";
+                }
+                else
+                {
+                    StreamWriter sw = file.CreateText();
+                    for (int i = 2; i < request.Length; i++)
+                    {
+                        sw.Write(request[i] + " ");
+                    }
+                    sw.Close();
+                    response += "200";
+                }
+            }
+            else if (request[0] == "GET")
+            {
+                FileInfo file = new FileInfo(filepath);
+                if (file.Exists)
+                {
+                    response += "200 " + File.ReadAllText(filepath);
+                }
+                else
+                {
+                    response += "404";
+                }
+            }
+            else if (request[0] == "DELETE")
+            {
+                FileInfo file = new FileInfo(filepath);
+                if (file.Exists)
+                {
+                    file.Delete();
+                    response += "200";
+                }
+                else
+                {
+                    response += "404";
+                }
+            }
+        }
+        else
+        {
+            response = "Invalid command";
+        }
+
+        // Отправляем результат клиенту
+        byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+        await client.SendAsync(responseBytes, SocketFlags.None);
+        Console.WriteLine($"Result sent to client: {response}\n");
+    }
+}
---
+++ SE_Lab3_1_client/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class Client
+{
+    static async Task Main(string[] args)
+    {
+        using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        try
+        {
+            await socket.ConnectAsync("127.0.0.1", 8888);
+            Console.WriteLine($"Connection to {socket.RemoteEndPoint} established");
+            string userInput = "";
+            int action;
+            do
+            {
+
+                Console.Write("Enter action (1 - get a file, 2 - create a file, 3 - delete a file, exit to quit): > ");
+                userInput = Console.ReadLine().ToLower();
+
+                if (userInput == "exit")
+                {
+                    // Отправляем команду exit и завершаем цикл
+                    byte[] exitBytes = Encoding.UTF8.GetBytes("exit");
+                    await socket.SendAsync(exitBytes, SocketFlags.None);
+                    Console.WriteLine("Exit command sent.");
+                    return;
+                }
+
+                if (!int.TryParse(userInput, out action) ||  action < 1 || action > 3)
+                {
+                    Console.WriteLine("Invalid action. Please enter 1, 2, 3, or exit.");
+
+                }
+            } while (!int.TryParse(userInput, out action) || action < 1 || action > 3);
+
+
+            Console.Write("Enter filename: > ");
+            string filename = Console.ReadLine();
+
+            string content = "";
+            if (action == 2)
+            {
+                Console.Write("Enter file content: > ");
+                content = Console.ReadLine();
+            }
+
+            string request = "";
+            if (action == 1)
+            {
+                request += "GET " + filename;
+            }
+            else if (action == 2)
+            {
+                request += "PUT " + filename + " " + content;
+            }
+            else if (action == 3)
+            {
+                request += "DELETE " + filename;
+            }
+
+            var requestBytes = Encoding.UTF8.GetBytes(request);
+            await socket.SendAsync(requestBytes, SocketFlags.None);
+            Console.WriteLine("The request was sent.");
+
+            byte[] buffer = new byte[512];
+            int bytesReceived = await socket.ReceiveAsync(buffer, SocketFlags.None);
+            string response = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+
+            if (action == 2)
+            {
+                if (response == "403")
+                {
+                    Console.WriteLine("The response says that file already exists!");
+                }
+                else
+                {
+                    Console.WriteLine("The response says that file was created!");
+                }
+            }
+            else if (action == 1)
+            {
+                if (response.StartsWith("200"))
+                {
+                    string[] parts = response.Split(' ');
+                    Console.WriteLine($"The content of the file is: {parts[1]}");
+                }
+                else if (response == "404")
+                {
+                    Console.WriteLine("The response says that the file was not found!");
+                }
+            }
+            else if (action == 3)
+            {
+                if (response == "200")
+                {
+                    Console.WriteLine("The response says that the file was successfully deleted!");
+                }
+                else if (response == "404")
+                {
+                    Console.WriteLine("The response says that the file was not found!");
+                }
+            }
+
+        }
+        catch (SocketException)
+        {
+            Console.WriteLine($"Failed to connect to {socket.RemoteEndPoint}");
+        }
+    }
+}
